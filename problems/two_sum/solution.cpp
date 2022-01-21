b54252e060db28c0c9bfaf9ed0d5d4da75d54ade
Tyler Pease <peaset.x1@gmail.com>
2022-01-21 16:50:58 +0000

    Sync LeetCode submission - Two Sum (cpp)

---
+++ problems/two_sum/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        for (int i = 0; i < nums.size(); i++){
+            int t = target - nums[i];
+            auto r = m.equal_range(t);
+            for (auto it = r.first; it != r.second; it++){
+                if (it->second != i && it->first == t){
+                    return {i, it->second};
+                }
+            }
+            m[nums[i]] = i;
+        }
+        
+        return {0,0};
+    }
+};
