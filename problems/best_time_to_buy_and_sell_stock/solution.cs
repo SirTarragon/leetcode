ec141c369949826f2cb348fafaae3a0a3f395f7b
Tyler Pease <peaset.x1@gmail.com>
2023-03-24 16:19:37 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (csharp)

---
+++ problems/best_time_to_buy_and_sell_stock/solution.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int val = prices[0];
+        int profit = 0;
+
+        foreach(int i in prices) {
+            if(i < val){ 
+                val = i; 
+            }
+            if(i-val>profit)profit=i-val;
+        }
+        
+        return profit;
+    }
+}
