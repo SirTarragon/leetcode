f116e873969cd7bbbbf96f6769c0644e21a4187a
Tyler Pease <peaset.x1@gmail.com>
2023-03-24 16:37:16 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (csharp)

---
+++ problems/intersection_of_two_arrays_ii/solution.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] Intersect(int[] nums1, int[] nums2) {
+        Dictionary<int, int> map = new Dictionary<int,int>();
+        List<int> intersect = new List<int>();
+
+        for(int i = 0; i < nums1.Length; i++) {
+            if(map.ContainsKey(nums1[i]))
+                map[nums1[i]]++;
+            else
+                map.Add(nums1[i], 1);
+        }
+
+        for(int i = 0; i < nums2.Length; i++) {
+            if(map.ContainsKey(nums2[i])) {
+                intersect.Add(nums2[i]);
+                if(--map[nums2[i]] == 0)
+                    map.Remove(nums2[i]);
+            }
+        }
+
+        return intersect.ToArray();
+    }
+}
