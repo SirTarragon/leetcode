cbb8c24fe6888a82aecd3bcd7fb402f395e2e920
Tyler Pease <peaset.x1@gmail.com>
2023-03-24 16:40:46 +0000

    Sync LeetCode submission - Is Subsequence (csharp)

---
+++ problems/is_subsequence/solution.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        if(s.Length > t.Length) return false;
+        if(String.IsNullOrEmpty(s)) return true;
+
+        int checkValue = 0;
+        for(int i = 0; checkValue < s.Length && i < t.Length; i++) {
+            if(s[checkValue] == t[i]) {
+                checkValue++;
+            }
+        }
+
+        return checkValue == s.Length;
+    }
+}
