79cce3c00035727df4940dbbbc04ad47fa0ae1b6
Tyler Pease <peaset.x1@gmail.com>
2023-03-28 20:45:18 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (csharp)

--- problems/median_of_two_sorted_arrays/solution.cs
+++ problems/median_of_two_sorted_arrays/solution.cs
@@ -1,9 +1,6 @@
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] combined = new int[nums1.Length + nums2.Length];
-        Array.Copy(nums1, combined, nums1.Length);
-        Array.Copy(nums2, 0, combined, nums1.Length, nums2.Length);
-        Array.Sort(combined);
+        int[] combined = MergeTwoSortedArrays(nums1, nums2);
 
         int middleIndex = (int)Math.Ceiling((double)(combined.Length/2));
         if(combined.Length % 2 != 0) {
@@ -11,4 +8,33 @@ public class Solution {
         }
         return (double)(combined[middleIndex-1] + combined[middleIndex])/2;
     }
+
+    public static int[] MergeTwoSortedArrays(int[] arr1, int[] arr2) {
+			int n = arr1.Length;
+			int m = arr2.Length;
+			int[] merge = new int[n + m];
+			int i = 0, j = 0, k = 0;
+			while (i < n && j < m) {
+				if (arr1[i] < arr2[j]) {
+					merge[k] = arr1[i];
+					i++;
+					k++;
+				} else {
+					merge[k] = arr2[j];
+					j++;
+					k++;
+				}
+			}
+			while (i < n) {
+				merge[k] = arr1[i];
+				i++;
+				k++;
+			}
+			while (j < m) {
+				merge[k] = arr2[j];
+				j++;
+				k++;
+			}
+			return merge;
+	}
 }
