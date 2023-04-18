3f7772c52ccace168f6053c46ae2605955899d74
Tyler Pease <peaset.x1@gmail.com>
2023-04-18 06:39:40 +0000

    Sync LeetCode submission - Missing Number (cpp)

--- problems/missing_number/solution.cpp
+++ problems/missing_number/solution.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int expSum = 0;
-        int runSum = 0;
+        int res = 0;
         for(int i = 0; i < nums.size(); i++) {
-            expSum += i+1;
-            runSum += nums[i];
+            res ^= i+1;
+            res ^= nums[i];
         }
-        return expSum - runSum;
+        return res;
     }
 };
