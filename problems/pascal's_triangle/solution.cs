92d2f47a278feb2db4db0ef5fae445ae6ebb03d4
Tyler Pease <peaset.x1@gmail.com>
2023-04-23 17:54:12 +0000

    Sync LeetCode submission - Pascal's Triangle (csharp)

--- problems/pascal's_triangle/solution.cs
+++ problems/pascal's_triangle/solution.cs
@@ -1,18 +1,13 @@
 public class Solution {
     public IList<IList<int>> Generate(int numRows) {
         IList<IList<int>> pascal = new List<IList<int>>();
-        int c = 1;
         for(int i = 0; i < numRows; i++) {
-            IList<int> curr = new List<int>();
-            for(int j = 0; j <= i; j++) {
-                if(j == 0 || i == 0) {
-                    c = 1;
-                } else {
-                    c = c * (i - j + 1)/j;
-                }
-                curr.Add(c);
+            IList<int> row = new List<int>() {1};
+            for(int k = 1; k < i; k++){
+                row.Add(pascal[i - 1][k - 1] + pascal[i - 1][k]);
             }
-            pascal.Add(curr);
+            if(i > 0) row.Add(1);
+            pascal.Add(row);
         }
         return pascal;
     }
