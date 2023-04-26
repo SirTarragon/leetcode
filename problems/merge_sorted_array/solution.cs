e7200b19735767dd611a07301aa5dc1898bf463e
Tyler Pease <peaset.x1@gmail.com>
2023-04-26 19:22:23 +0000

    Sync LeetCode submission - Merge Sorted Array (csharp)

--- problems/merge_sorted_array/solution.cs
+++ problems/merge_sorted_array/solution.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i = m, j = 0; i < m+n; i++, j++) {
-            nums1[i] = nums2[j];
+        for(int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
         } 
         Array.Sort(nums1);
     }
