41b535ee12fb3765e86e4c04527d2444d5d5d5cc
Tyler Pease <peaset.x1@gmail.com>
2023-04-28 03:53:23 +0000

    Sync LeetCode submission - Reverse Words in a String (csharp)

--- problems/reverse_words_in_a_string/solution.cs
+++ problems/reverse_words_in_a_string/solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public string ReverseWords(string s) {
-        return string.Join(' ', (s.Split().Where(s => !string.IsNullOrWhiteSpace(s))).Reverse()).Trim();
+        string[] array = s.Split(' ');
+        StringBuilder reverse = new StringBuilder();
+
+        for(int i = array.Length - 1; i >= 0; i--) {
+            if(!string.IsNullOrEmpty(array[i])) {
+                reverse.Append(" " + array[i]);
+            }
+        }
+
+        return reverse.ToString().Trim();
     }
 }
