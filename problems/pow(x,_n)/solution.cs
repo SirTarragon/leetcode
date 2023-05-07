83887893dd51ba166e9a5682ff61d4102b4317c1
Tyler Pease <peaset.x1@gmail.com>
2023-05-07 21:45:32 +0000

    Sync LeetCode submission - Pow(x, n) (csharp)

---
+++ problems/pow(x,_n)/solution.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public double MyPow(double x, int n) {
+        double r = 1;
+        long N = n;
+
+        if(N < 0) {
+            N *= -1;
+            x = 1 / x;
+        }
+        
+        if(N > 0 || N < 0)
+            while(N != 0) {
+                if((N & 1) == 1)
+                    r *= x;
+
+                x *= x;
+                N >>= 1;
+            }
+
+        return r;
+    }
+}
