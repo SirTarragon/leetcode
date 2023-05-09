d42f73a3e10b385d2c437c44f7fa5a95aea3402c
Tyler Pease <peaset.x1@gmail.com>
2023-05-09 22:13:11 +0000

    Sync LeetCode submission - Three Consecutive Odds (c)

--- problems/three_consecutive_odds/solution.c
+++ problems/three_consecutive_odds/solution.c
@@ -1,8 +1,8 @@
 bool threeConsecutiveOdds(int* arr, int arrSize){
     if(arrSize < 3) return false;
-    long product = arr[0] * arr[1] * arr[2];
+    int product = ((arr[0] % 2) + 2) * ((arr[1] % 2) + 2) * ((arr[2] % 2) + 2);
     if(product % 2 == 1) return true;
     for(int i = 3; i < arrSize && product % 2 != 1; i++)
-        product = (product * arr[i]) / arr[i-3];
-    return product % 2 == 1;
+        product = (product * ((arr[i] % 2) + 2)) / ((arr[i-3] % 2) + 2);
+    return product %  2 == 1;
 }
