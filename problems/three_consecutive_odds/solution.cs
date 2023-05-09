da0b272e6c240457678238312df07113a3eb4287
Tyler Pease <peaset.x1@gmail.com>
2023-05-09 18:18:14 +0000

    Sync LeetCode submission - Three Consecutive Odds (csharp)

--- problems/three_consecutive_odds/solution.cs
+++ problems/three_consecutive_odds/solution.cs
@@ -1,8 +1,7 @@
 public class Solution {
     public bool ThreeConsecutiveOdds(int[] arr) {
-        int count = 0;
-        for(int i = 0; i < arr.Length && count != 3; i++)
-            count = (arr[i] % 2 == 1) ? ++count : 0;
-        return count == 3;
+        for(int i = 0; i < arr.Length - 2; i++)
+            if(arr[i] % 2 == 1 && arr[i+1] % 2 == 1 && arr[i+2] % 2 == 1) return true;
+        return false;
     }
 }
