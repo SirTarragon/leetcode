6f3e514c96611016e368eb6a859139a08ca805f7
Tyler Pease <peaset.x1@gmail.com>
2023-06-08 23:36:30 +0000

    Sync LeetCode submission - K Items With the Maximum Sum (csharp)

--- problems/k_items_with_the_maximum_sum/solution.cs
+++ problems/k_items_with_the_maximum_sum/solution.cs
@@ -2,12 +2,14 @@ public class Solution {
     public int KItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
         int maxSum = 0;
         for(int i = 0; i < k; i++) {
-            if(numOnes-- > 0) {
+            if(numOnes > 0) {
                 maxSum++;
-            } else if(numZeros-- > 0) {
-                maxSum += 0;
-            } else if(numNegOnes-- > 0) {
+                numOnes--;
+            } else if(numZeros > 0) {
+                numZeros--;
+            } else if(numNegOnes > 0) {
                 maxSum--;
+                numNegOnes--;
             }
         }
         return maxSum;
