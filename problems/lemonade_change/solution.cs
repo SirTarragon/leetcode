d02a3164a8a63c313559d3c9a5913977f3969787
Tyler Pease <peaset.x1@gmail.com>
2023-06-29 03:46:38 +0000

    Sync LeetCode submission - Lemonade Change (csharp)

---
+++ problems/lemonade_change/solution.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool LemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        foreach(int bill in bills) {
+            if (bill == 5)
+                five++;
+            else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
